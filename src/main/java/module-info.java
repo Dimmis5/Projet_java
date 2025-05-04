613b7f6378aa3f22916650f061c6ebd1277fddc9
Dimmis5 <zhangdimeo@gmail.com>
2025-05-04 14:58:32 +0200

    Ini projet java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.projet_java {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.projet_java to javafx.fxml;
+    exports org.example.projet_java;
+}
