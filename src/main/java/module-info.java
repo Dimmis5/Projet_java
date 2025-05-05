927b7d94009584db71e3fa1944df398dec9e7add
Dimmis5 <zhangdimeo@gmail.com>
2025-05-05 11:51:24 +0200

    Continuation de la page Accueil en java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.projet_java {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jdk.incubator.vector;
 
 
     opens org.example.projet_java to javafx.fxml;
--- src/main/java/org/example/projet_java/Statistiques.java
+++ src/main/java/org/example/projet_java/Statistiques.java
@@ -3,11 +3,11 @@ package org.example.projet_java;
 import java.util.HashMap;
 
 public class Statistiques {
-    HashMap<Salle,double> utilisationSalle;
-    HashMap<Enseignant,double> utilisationEnseignant;
+    HashMap<Salle, Double> utilisationSalle;
+    HashMap<Enseignant, Double> utilisationEnseignant;
 
     public Statistiques() {
-        this.utilisationSalle = new HashMap<Salle,double>();
-        this.utilisationEnseignant = = new HashMap<Enseignant, double>();
+        this.utilisationSalle = new HashMap<Salle, Double>();
+        this.utilisationEnseignant = new HashMap<Enseignant, Double>();
     }
 }
