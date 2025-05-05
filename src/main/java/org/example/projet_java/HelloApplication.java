58c621da76406ca02ac63c922daecf0da61642e4
Dimmis5 <zhangdimeo@gmail.com>
2025-05-05 11:16:33 +0200

    Création de la page Accueil en javaFx

--- src/main/java/org/example/projet_java/HelloApplication.java
+++ src/main/java/org/example/projet_java/HelloApplication.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,13 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Button Etudiant = new Button("Etudiant");
-        Button Professeur = new Button("Professuer");
-        Button Administrateur = new Button("Administrateur");
-
-        VBox button = new VBox(10, Etudiant);
 
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
