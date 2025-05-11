c3eb08205e5d81ba87a580213f3ff18bcd9ebead
Dimmis5 <zhangdimeo@gmail.com>
2025-05-11 23:04:29 +0200

    Avancement sur les tests : Etudiant, EtudiantTest, HoraireTest, NotificationTest, SalleTest, StatistiquesTest

--- src/test/java/org/example/projet_java/HoraireTest.java
+++ src/test/java/org/example/projet_java/HoraireTest.java
@@ -7,26 +7,41 @@ import static org.junit.jupiter.api.Assertions.*;
 class HoraireTest {
 
     @Test
-    void getJour() {
+    public void testConstructeur(){
+        Horaire horaire = new Horaire("Lundi", "8h00", "10h00");
+        assertNotNull(horaire);
     }
 
     @Test
-    void setJour() {
+    public void testGetters(){
+        Horaire horaire = new Horaire("Lundi", "8h00", "10h00");
+
+        assertEquals("Lundi", horaire.getJour());
+        assertEquals("8h00", horaire.getHeure_debut());
+        assertEquals("10h00", horaire.getHeure_fin());
     }
 
     @Test
-    void getHeure_fin() {
+    public void testSetJour(){
+        Horaire horaire = new Horaire("Lundi", "8h00", "10h00");
+        horaire.setJour("Mardi");
+
+        assertEquals("Mardi", horaire.getJour());
     }
 
     @Test
-    void setHeure_fin() {
+    public void testSetHeureDebut(){
+        Horaire horaire = new Horaire("Lundi", "8h00", "10h00");
+        horaire.setHeure_debut("9h00");
+
+        assertEquals("9h00", horaire.getHeure_debut());
     }
 
     @Test
-    void getHeure_debut() {
-    }
+    public void testSetHeureFin(){
+        Horaire horaire = new Horaire("Lundi", "8h00", "10h00");
+        horaire.setHeure_fin("11h00");
 
-    @Test
-    void setHeure_debut() {
+        assertEquals("11h00", horaire.getHeure_fin());
     }
 }
