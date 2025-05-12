12c0cdc90ada12e95498c628b9fc62c032f82964
Dimmis5 <zhangdimeo@gmail.com>
2025-05-12 20:20:48 +0200

    Avancement sur les tests :Utilisateur

--- src/test/java/org/example/projet_java/UtilisateurTest.java
+++ src/test/java/org/example/projet_java/UtilisateurTest.java
@@ -7,38 +7,50 @@ import static org.junit.jupiter.api.Assertions.*;
 class UtilisateurTest {
 
     @Test
-    void getId() {
+    public void testConstructeur(){
+        Utilisateur utilisateur = new Utilisateur(41, "Maltre", "Baptiste", "mb@gmail.com", "mb123");
+
+        assertNotNull(utilisateur);
+        assertEquals(41, utilisateur.getId());
+        assertEquals("Maltre", utilisateur.getNom());
+        assertEquals("Baptiste", utilisateur.getPrenom());
+        assertEquals("mb@gmail.com", utilisateur.getMail());
+        assertEquals("mb123", utilisateur.getMdp());
     }
 
     @Test
-    void setId() {
+    public void testGet(){
+        Utilisateur utilisateur = new Utilisateur(41, "Maltre", "Baptiste", "mb@gmail.com", "mb123");
+
+        assertEquals(41, utilisateur.getId());
+        assertEquals("Maltre", utilisateur.getNom());
+        assertEquals("Baptiste", utilisateur.getPrenom());
+        assertEquals("mb@gmail.com", utilisateur.getMail());
+        assertEquals("mb123", utilisateur.getMdp());
     }
 
     @Test
-    void getPrenom() {
+    public void testSet(){
+        Utilisateur utilisateur = new Utilisateur(41, "Maltre", "Baptiste", "mb@gmail.com", "mb123");
+
+        utilisateur.setId(7);
+        utilisateur.setNom("Popa");
+        utilisateur.setPrenom("Pierre");
+        utilisateur.setMail("Pp@gmail.com");
+
+        assertEquals(7, utilisateur.getId());
+        assertEquals("Popa", utilisateur.getNom());
+        assertEquals("Pierre", utilisateur.getPrenom());
+        assertEquals("Pp@gmail.com", utilisateur.getMail());
     }
 
     @Test
-    void setPrenom() {
-    }
+    public void testConsulterReturnsSameEmploi(){
+        Utilisateur utilisateur = new Utilisateur(41, "Maltre", "Baptiste", "mb@gmail.com", "mb123");
+        EmploiDuTemps edt = new EmploiDuTemps(99);
 
-    @Test
-    void getNom() {
-    }
+        EmploiDuTemps retourne = utilisateur.consulter(edt);
 
-    @Test
-    void setNom() {
-    }
-
-    @Test
-    void getMail() {
-    }
-
-    @Test
-    void setMail() {
-    }
-
-    @Test
-    void consulter() {
+        assertSame(edt, retourne);
     }
 }
