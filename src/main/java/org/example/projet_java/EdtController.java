7225c146aa3a84896035ebbe99ec1b97f89f9aa5
Oriane5 <orco63803@eleve.isep.fr>
2025-05-14 08:37:43 +0200

    connexion avec csv

--- src/main/java/org/example/projet_java/EdtController.java
+++ src/main/java/org/example/projet_java/EdtController.java
@@ -18,29 +18,45 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class EdtController {
+    // Il faudra modifier et mettre le csv dans ressource
+    // Ajouter dans le csv l'administrateur en faire un autre ???
     private final String CSV = "C:\\Users\\Oriane\\OneDrive - ISEP\\ALGORITHMIQUE ET PROGRAMMATION\\csv.csv";
 
     @FXML
     public void connexionetudiant(ActionEvent event) {
         Label identifiant = new Label("Identifiant");
         TextField textidentifiant = new TextField();
-        String id_etudiant = textidentifiant.getText();
+
+        Label mdp = new Label("Mot de passe");
+        TextField textmdp = new TextField();
+
+        Button connexion = new Button("Se connecter");
+
+        connexion.setOnAction(e -> {
+            String id_etudiant = textidentifiant.getText();
+            String mdp_etudiant = textmdp.getText();
+
+            try (BufferedReader br = new BufferedReader(new FileReader(CSV))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] valeurs = line.split(";");
+                    if (valeurs[0].equals(id_etudiant) && valeurs[4].equals(mdp_etudiant)) {
+                        edt();
+                        break;
+                    }
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        });
 
         HBox hidentifiant = new HBox(20);
         hidentifiant.setAlignment(Pos.CENTER);
         hidentifiant.getChildren().addAll(identifiant, textidentifiant);
 
-        Label mdp = new Label("Mot de passe");
-        TextField textmdp = new TextField();
-        String mdp_etudiant = textmdp.getText();
-
         HBox hmdp = new HBox(20);
         hmdp.setAlignment(Pos.CENTER);
         hmdp.getChildren().addAll(mdp, textmdp);
-
-        Button connexion = new Button("Se connecter");
-
-        System.out.println(textidentifiant.getText()+ " " + mdp_etudiant);
 
         VBox layout = new VBox(20);
         layout.setAlignment(Pos.CENTER);
@@ -60,34 +76,37 @@ public class EdtController {
     public void connexionenseignant(ActionEvent event) {
         Label identifiant = new Label("Identifiant");
         TextField textidentifiant = new TextField();
-        String id_enseignant = textidentifiant.getText();
+
+        Label mdp = new Label("Mot de passe");
+        TextField textmdp = new TextField();
+
+        Button connexion = new Button("Se connecter");
+
+        connexion.setOnAction(e -> {
+            String id_enseignant = textidentifiant.getText();
+            String mdp_enseignant = textmdp.getText();
+
+            try (BufferedReader br = new BufferedReader(new FileReader(CSV))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] valeurs = line.split(";");
+                    if (valeurs[16].equals(id_enseignant) && valeurs[20].equals(mdp_enseignant)) {
+                        edt();
+                        break;
+                    }
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        });
 
         HBox hidentifiant = new HBox(20);
         hidentifiant.setAlignment(Pos.CENTER);
         hidentifiant.getChildren().addAll(identifiant, textidentifiant);
 
-        Label mdp = new Label("Mot de passe");
-        TextField textmdp = new TextField();
-        String mdp_enseignant = textmdp.getText();
-
         HBox hmdp = new HBox(20);
         hmdp.setAlignment(Pos.CENTER);
         hmdp.getChildren().addAll(mdp, textmdp);
-
-        Button connexion = new Button("Se connecter");
-        try (BufferedReader br = new BufferedReader(new FileReader(CSV))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] valeurs = line.split(";");
-                if (valeurs[16].equals(id_enseignant) && valeurs[20].equals(mdp_enseignant)) {
-                    connexion.setOnAction(ouvrir -> {
-                        edt();
-                    });
-                }
-            }
-        }catch(IOException ioe){
-            ioe.printStackTrace();
-        }
 
         VBox layout = new VBox(20);
         layout.setAlignment(Pos.CENTER);
