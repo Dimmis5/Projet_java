b7b4a3d72a00fd1b78108b5d28ae4036c0d8a3eb
Oriane5 <orco63803@eleve.isep.fr>
2025-05-17 16:59:52 +0200

    affichage emploi du temps

--- src/main/java/org/example/projet_java/EdtController.java
+++ src/main/java/org/example/projet_java/EdtController.java
@@ -28,7 +28,7 @@ import java.util.Calendar;
 import java.util.List;
 
 public class EdtController {
-    private final String CSV = "/home/dimmis/Téléchargements/csv (1).csv";
+    private final String CSV = "C:\\Users\\Oriane\\OneDrive - ISEP\\ALGORITHMIQUE ET PROGRAMMATION\\csv.csv";
 
     @FXML
     public void connexionetudiant(ActionEvent event) {
@@ -133,52 +133,117 @@ public class EdtController {
 
     @FXML
     public void edtEnseignant(String id_enseignant) {
-        LocalDate dateActuelle = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String dateFormatee = dateActuelle.format(formatter);
-        System.out.println(dateFormatee);
-
         VBox layout = new VBox(20);
         layout.setAlignment(Pos.TOP_CENTER);
+        layout.setPadding(new Insets(20));
 
-        HBox semainesSection = new HBox(10);
-        semainesSection.setAlignment(Pos.CENTER);
-
-        for (int i = 1; i <= 30; i++) {
-            final int semaineNumber = i;
-            Button semaineButton = new Button(String.valueOf(i));
-            // Ajouter action bouton
-            semainesSection.getChildren().add(semaineButton);
+        LocalDate aujourdhui = LocalDate.now();
+        LocalDate lundi = aujourdhui.with(DayOfWeek.MONDAY);
+        List<String> semaine = new ArrayList<>();
+        DateTimeFormatter dateformatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        for (int i = 0; i < 7; i++) {
+            semaine.add(lundi.plusDays(i).format(dateformatter));
         }
 
-        layout.getChildren().add(semainesSection);
+        String[] heures = {"8h00", "9h00", "10h00", "11h00", "12h00", "13h00", "14h00", "15h00", "16h00", "17h00", "18h00", "19h00", "20h00"};
+
+        Label titreSemaine = new Label("Semaine du " + semaine.get(0) + " au " + semaine.get(6));
+        layout.getChildren().add(titreSemaine);
+
+        List<String[]> coursList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(CSV))) {
+            String line;
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] cours = line.split(";");
+                if (cours[16].equals(id_enseignant)) {
+                    coursList.add(cours);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         GridPane agendaGrid = new GridPane();
-        agendaGrid.setHgap(20);
-        agendaGrid.setVgap(20);
+        agendaGrid.setHgap(10);
+        agendaGrid.setVgap(10);
+        agendaGrid.setPadding(new Insets(10));
 
-        String[] jours = {"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"};
-        for (int i = 0; i < jours.length; i++) {
-            Label jourLabel = new Label(jours[i]);
+        for (int i = 0; i < 6; i++) {
+            Label jourLabel = new Label(semaine.get(i));
             agendaGrid.add(jourLabel, i + 1, 0);
         }
 
-        for (int i = 8; i <= 20; i++) {
-            Label heureLabel = new Label(i + ":00");
-            agendaGrid.add(heureLabel, 0, i - 7);
+        for (int heure = 0; heure <= 12; heure++) {
+            Label heureLabel = new Label(heures[heure]);
+            agendaGrid.add(heureLabel, 0, heure + 1);
+        }
 
-            for (int j = 0; j < jours.length; j++) {
-                Button cours = new Button("");
-                cours.setPrefWidth(100);
-                agendaGrid.add(cours, j + 1, i - 7);
+        Button[][] cellules = new Button[6][13];
+        for (int jour = 0; jour < 6; jour++) {
+            for (int heure = 0; heure <= 12; heure++) {
+                Button celluleVide = new Button();
+                celluleVide.setPrefWidth(100);
+                celluleVide.setPrefHeight(30);
+                agendaGrid.add(celluleVide, jour + 1, heure + 1);
+                cellules[jour][heure] = celluleVide;
+            }
+        }
+
+        for (String[] cours : coursList) {
+            String dateCours = cours[12];
+            String heureCours = cours[13];
+            String heurefinCours = cours[14];
+            String matiere = cours[11];
+            String classe = cours[25];
+            String salle = cours[6];
+
+            int jourIndex = -1;
+            for (int i = 0; i < semaine.size(); i++) {
+                if (semaine.get(i).equals(dateCours)) {
+                    jourIndex = i;
+                    break;
+                }
+            }
+
+            int heureIndex = -1;
+            for (int i = 0; i < heures.length; i++) {
+                if (heures[i].equals(heureCours)) {
+                    heureIndex = i;
+                    break;
+                }
+            }
+
+            if (jourIndex >= 0 && jourIndex < 6 && heureIndex >= 0 && heureIndex <= 12) {
+                Button coursButton = new Button(matiere);
+                coursButton.setPrefWidth(100);
+                coursButton.setPrefHeight(30);
+
+                agendaGrid.getChildren().remove(cellules[jourIndex][heureIndex]);
+                agendaGrid.add(coursButton, jourIndex + 1, heureIndex + 1);
+
+                final String matiereInfo = matiere;
+                coursButton.setOnAction(e -> {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Détails du cours");
+                    alert.setHeaderText("Cours de " + matiereInfo);
+                    alert.setContentText("Date : " + dateCours
+                            + "\nHeure : " + heureCours + " - " + heurefinCours
+                            + "\nClasse : " + classe
+                            + "\nSalle : " + salle);
+                    alert.showAndWait();
+                });
             }
         }
 
         layout.getChildren().add(agendaGrid);
 
-        Scene scene = new Scene(layout, 400, 400);
+        ScrollPane scrollPane = new ScrollPane(layout);
+        scrollPane.setFitToWidth(true);
+
+        Scene scene = new Scene(scrollPane, 700, 600);
         Stage stage = new Stage();
-        stage.setTitle("Emploi du Temps");
+        stage.setTitle("Emploi du Temps - " + id_enseignant);
         stage.setScene(scene);
         stage.show();
     }
@@ -228,34 +293,91 @@ public class EdtController {
 
         for (int heure = 0; heure <= 12; heure++) {
             Label heureLabel = new Label(heures[heure]);
-            agendaGrid.add(heureLabel, 0, heure);
+            agendaGrid.add(heureLabel, 0, heure + 1);
+        }
 
-            for (int jour = 0; jour < 6; jour++) {
-                for (String[] cours : coursList) {
-                    if (cours[12].equals(semaine.get(jour))) {
-                        if (cours[13].equals(heures[heure])) {
-                            Button coursButton = new Button(cours[11]);
-                            coursButton.setPrefWidth(100);
-                            agendaGrid.add(coursButton, jour + 1, heure);
-                        } else {
-                            Button coursButton = new Button();
-                            coursButton.setPrefWidth(100);
-                            agendaGrid.add(coursButton, jour + 1, heure);
-                        }
-                    } else {
-                        Button coursButton = new Button();
-                        coursButton.setPrefWidth(100);
-                        agendaGrid.add(coursButton, jour + 1, heure);
-                    }
+        Button[][] cellules = new Button[6][13];
+        for (int jour = 0; jour < 6; jour++) {
+            for (int heure = 0; heure <= 12; heure++) {
+                Button celluleVide = new Button();
+                celluleVide.setPrefWidth(100);
+                celluleVide.setPrefHeight(30);
+                agendaGrid.add(celluleVide, jour + 1, heure + 1);
+                cellules[jour][heure] = celluleVide;
+            }
+        }
+
+        for (String[] cours : coursList) {
+            String dateCours = cours[12];
+            String heureCours = cours[13];
+            String heurefinCours = cours[14];
+            String matiere = cours[11];
+            String enseignant_nom = cours[17];
+            String enseignant_prenom = cours[18];
+            String salle = cours[6];
+
+            int jourIndex = -1;
+            for (int i = 0; i < semaine.size(); i++) {
+                if (semaine.get(i).equals(dateCours)) {
+                    jourIndex = i;
+                    break;
                 }
+            }
+
+            int heureIndex = -1;
+            for (int i = 0; i < heures.length; i++) {
+                if (heures[i].equals(heureCours)) {
+                    heureIndex = i;
+                    break;
+                }
+            }
+
+            if (jourIndex >= 0 && jourIndex < 6 && heureIndex >= 0 && heureIndex <= 12) {
+                Button coursButton = new Button(matiere);
+                coursButton.setPrefWidth(100);
+                coursButton.setPrefHeight(30);
+
+                agendaGrid.getChildren().remove(cellules[jourIndex][heureIndex]);
+                agendaGrid.add(coursButton, jourIndex + 1, heureIndex + 1);
+
+                final String matiereInfo = matiere;
+                coursButton.setOnAction(e -> {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Détails du cours");
+                    alert.setHeaderText("Cours de " + matiereInfo);
+                    alert.setContentText("Date : " + dateCours
+                            + "\nHeure : " + heureCours + " - " + heurefinCours
+                            + "\nEnseignant : " + enseignant_nom + " " + enseignant_prenom
+                            + "\nSalle : " + salle);
+
+                    alert.showAndWait();
+                });
             }
         }
 
         layout.getChildren().add(agendaGrid);
 
-        Scene scene = new Scene(layout, 400, 400);
+        Label date = new Label("Date : ");
+        TextField text_date = new TextField();
+
+        HBox date_notification = new HBox(10);
+        date_notification.getChildren().addAll(date, text_date);
+
+        Label message = new Label("Message : ");
+        TextField text_message = new TextField();
+
+        HBox message_notification = new HBox(10);
+        message_notification.getChildren().addAll(message, text_message);
+
+        VBox VNotification = new VBox(10);
+        VNotification.getChildren().addAll(date_notification, message_notification);
+
+        ScrollPane scrollPane = new ScrollPane(layout);
+        scrollPane.setFitToWidth(true);
+
+        Scene scene = new Scene(scrollPane, 700, 600);
         Stage stage = new Stage();
-        stage.setTitle("Emploi du Temps");
+        stage.setTitle("Emploi du Temps - " + id_etudiant);
         stage.setScene(scene);
         stage.show();
     }
@@ -311,16 +433,27 @@ public class EdtController {
     }
 
     @FXML
-    public void edtAdmi(String id_administrateur){
-
+    public void edtAdmi(String id_administrateur) {
         VBox layout = new VBox(20);
         layout.setAlignment(Pos.TOP_CENTER);
+        layout.setPadding(new Insets(20));
+
 
         HBox semainesSection = new HBox(10);
         semainesSection.setAlignment(Pos.TOP_LEFT);
 
         ComboBox<String> etudiantsComboBox = new ComboBox<>();
         etudiantsComboBox.setPromptText("Choisir un étudiant");
+
+
+        LocalDate aujourdhui = LocalDate.now();
+        LocalDate lundi = aujourdhui.with(DayOfWeek.MONDAY);
+        List<String> semaine = new ArrayList<>();
+        DateTimeFormatter dateformatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        for (int i = 0; i < 7; i++) {
+            semaine.add(lundi.plusDays(i).format(dateformatter));
+        }
+
 
         try (BufferedReader br = new BufferedReader(new FileReader("/home/dimmis/Téléchargements/csv (1).csv"))) {
             String line;
@@ -345,35 +478,53 @@ public class EdtController {
 
         semainesSection.getChildren().add(etudiantsComboBox);
 
-        for (int i = 1; i <= 30; i++) {
-            final int semaineNumber = i;
-            Button semaineButton = new Button(String.valueOf(i));
-            semainesSection.getChildren().add(semaineButton);
+
+        String[] heures = {"8h00", "9h00", "10h00", "11h00", "12h00", "13h00", "14h00", "15h00", "16h00", "17h00", "18h00", "19h00", "20h00"};
+
+        Label titreSemaine = new Label("Semaine du " + semaine.get(0) + " au " + semaine.get(6));
+        layout.getChildren().add(titreSemaine);
+
+        List<String[]> coursList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(CSV))) {
+            String line;
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] cours = line.split(";");
+                if (cours[0].equals(id_administrateur)) {
+                    coursList.add(cours);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        layout.getChildren().add(semainesSection);
-
         GridPane agendaGrid = new GridPane();
-        agendaGrid.setHgap(20);
-        agendaGrid.setVgap(20);
+        agendaGrid.setHgap(10);
+        agendaGrid.setVgap(10);
+        agendaGrid.setPadding(new Insets(10));
 
-        String[] jours = {"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"};
-        for (int i = 0; i < jours.length; i++) {
-            Label jourLabel = new Label(jours[i]);
+        for (int i = 0; i < 6; i++) {
+            Label jourLabel = new Label(semaine.get(i));
             agendaGrid.add(jourLabel, i + 1, 0);
         }
 
-        for (int i = 8; i <= 20; i++) {
-            Label heureLabel = new Label(i + ":00");
-            agendaGrid.add(heureLabel, 0, i - 7);
+        for (int heure = 0; heure <= 12; heure++) {
+            Label heureLabel = new Label(heures[heure]);
+            agendaGrid.add(heureLabel, 0, heure + 1);
+        }
 
-            for (int j = 0; j < jours.length; j++) {
-                Button cours = new Button("");
-                cours.setPrefWidth(100);
-                agendaGrid.add(cours, j + 1, i - 7);
+        Button[][] cellules = new Button[6][13];
+        for (int jour = 0; jour < 6; jour++) {
+            for (int heure = 0; heure <= 12; heure++) {
+                Button celluleVide = new Button();
+                celluleVide.setPrefWidth(100);
+                celluleVide.setPrefHeight(30);
+                agendaGrid.add(celluleVide, jour + 1, heure + 1);
+                cellules[jour][heure] = celluleVide;
             }
         }
 
+        layout.getChildren().add(semainesSection);
         layout.getChildren().add(agendaGrid);
 
         Scene scene = new Scene(layout, 600, 400);
@@ -381,6 +532,5 @@ public class EdtController {
         stage.setTitle("Emploi du Temps");
         stage.setScene(scene);
         stage.show();
-
     }
 }
